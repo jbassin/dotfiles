3a325f32f0bd6d29724156a813c9a354fd98e3b1
Josh Bassin <josh.r.bassin@gmail.com>
2026-05-30 12:40:49 -0400

    init homebrew

--- run_000_init.sh
+++ run_000_init.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 if [[ ! -f /Users/jbassin/.local/bin/mise ]]; then
   curl https://mise.run | sh
 fi
